da5f0b94cd9eba5f633e99035ecedf4e48cd2500
Zeteny-Mato <koppany.mato.gbg22@amerikanskagymnasiet.se>
2024-05-08 11:49:18 +0200

    added pong and added stuff to rpg game

--- main.py
+++ main.py
@@ -9,14 +9,33 @@ screen = pygame.display.set_mode((width, height))
 
 pygame.display.set_caption("My RPG Game")
 
+player_image = pygame.image.load("Smaller_black_circle.png")
+player_rect = player_image.get_rect()
+player_rect.center = (width // 2, height // 2)
+
+player_speed = 1
+
 running = True
 
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
+    
+    keys = pygame.key.get_pressed()
+
+    if keys[pygame.K_LEFT]:
+        player_rect.move_ip(-player_speed, 0)
+    if keys[pygame.K_RIGHT]:
+        player_rect.move_ip(player_speed, 0)
+    if keys[pygame.K_UP]:
+        player_rect.move_ip(0, -player_speed)
+    if keys[pygame.K_DOWN]:
+        player_rect.move_ip(0, player_speed)
 
     screen.fill((255, 255, 255))
+
+    screen.blit(player_image, player_rect)
 
     pygame.display.flip()
 
